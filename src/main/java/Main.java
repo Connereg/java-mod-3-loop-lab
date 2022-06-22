4f738a5ea33d3aa9af21be2ddd8d364a5415af00
Conner Gleason <connereg@optonline.net>
2022-06-22 15:04:52 -0400

    Completed Assignment, VSCode Errors still existant

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,39 @@ public class Main {
         int targetYear = 2011;
         int currentYear = startingYear;
         while (currentYear < targetYear) {
-            System.out.println((currentYear - startingYear) + " year(s) have passed");
+            if (currentYear != startingYear) {
+               int yearDifference = (currentYear - startingYear);
+               String sentenceEnder = "";
+               if (yearDifference <= 1) {
+                   sentenceEnder = " year has passed";
+               }
+               else {
+                   sentenceEnder = " years have passed";
+               }
+                System.out.println((yearDifference) +  sentenceEnder);
+            } 
+            // else {
+            //     System.out.println("Current year and Starting year are the same!");
+            // }
+
             // conditional logic based on the current year
             currentYear++;
+        }
+        currentYear = startingYear;
+
+        for (int i = currentYear; i < targetYear; i++) {
+            if (i != startingYear) {
+                int yearDifferenceLoop = (i - startingYear);
+                String sentenceEnderLoop = "";
+                if (yearDifferenceLoop == 1){
+                    sentenceEnderLoop = " year has passed";
+                }
+                else {
+                    sentenceEnderLoop = " years have passed";
+                }
+                System.out.println((yearDifferenceLoop) + sentenceEnderLoop);
+                // System.out.println((i - startingYear) + " year(s) have passed");
+            }
         }
     }
 }
